0afe80d814b2fa84a9795f4db9bdaccf80f3792c
latyas(懒) <latyas@gmail.com>
2015-12-27 20:43:38 +0800

    Update config.js

--- javascripts/config.js
+++ javascripts/config.js
@@ -6,13 +6,3 @@ var sitetitle = 'Robot Notes';						//Name of your blog.
 var rss = '';									//Rss address of your blog.
 var suffix = '';								//Suffix of post files, '.' should be included.
 var branch = '';								//Branch of your blog repository. 'master' is default if you left it blank.
-
-//Sample Config:
-//var disqus_shortname = 'sneezry';
-//var hostbase = 'http://sneezry.com/blog';
-//var githubname = 'sneezry';
-//var repos = 'blog';	
-//var sitetitle = 'Sneezry';
-//var rss = '';
-//var suffix = '.md';
-//var branch = 'gh-pages';
