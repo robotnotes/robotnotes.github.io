6853e5d90ed9d5714b8e1c186bc32b527676f1aa
latyas(懒) <latyas@gmail.com>
2016-12-15 04:27:49 +0800

    Update config.js

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,8 +1,8 @@
 var disqus_shortname = 'diaosi2008';			//Short Name of disqus, this parameter is for disqus comment box.
-var hostbase = 'http://robotnotes.github.io';		//The absolute root address of your blog without '/' in the end.
+var hostbase = 'http://roboticnotes.xyz';		//The absolute root address of your blog without '/' in the end.
 var githubname = 'robotnotes';						//Your username of GitHub.
 var repos = 'robotnotes.github.io';					//Name of repository that contains your blog.
-var sitetitle = 'Robot Notes';						//Name of your blog.
+var sitetitle = '机工学笔记（尴尬';						//Name of your blog.
 var rss = '';									//Rss address of your blog.
 var suffix = '';								//Suffix of post files, '.' should be included.
 var branch = '';								//Branch of your blog repository. 'master' is default if you left it blank.
